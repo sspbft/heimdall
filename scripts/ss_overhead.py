e80a022d9de83d608e48b01df15a170893461670
Axel Niklasson <axel.niklasson@live.com>
2019-04-05 16:00:33 +0200

    Extend ss overhead script

--- scripts/ss_overhead.py
+++ scripts/ss_overhead.py
@@ -10,56 +10,40 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 EXPERIMENT = "ss_overhead"
-PROM_QUERY = (f"bottomk({api.get_number_of_byz()+1}, client_req_exec_time) by (state_length)")
+Q_EXEC_TIME = f"max(bottomk({api.get_number_of_byz()+1}, client_req_exec_time) by (state_length)) by (state_length)"
+Q_MSGS_SENT = f"sum(msgs_during_exp) by (state_length)"
+Q_BYTES_SENT = f"sum(bytes_during_exp) by (state_length)"
 
-def transform(time_series):
+def transform(exec_time_series, msgs_sent_time_series, bts_sent_time_series):
     data_points = []
     # sort data points ASC wrt state length, gives us client_reqs in ASC order
-    data_asc = sorted(time_series, key=lambda m: float(m["metric"]["state_length"]))
+    exec_time_asc = sorted(exec_time_series, key=lambda m: float(m["metric"]["state_length"]))
+    msgs_sent_asc = sorted(msgs_sent_time_series, key=lambda m: float(m["metric"]["state_length"]))
+    bts_sent_asc = sorted(bts_sent_time_series, key=lambda m: float(m["metric"]["state_length"]))
     data_dct = {}
-    for el in data_asc:
-        x = int(el["metric"]["state_length"])
-        if x in data_dct:
-            if float(el["value"][1]) > data_dct[x].get("exec_time"):
-                y = float(el["value"][1])
-                total_msgs = int(el["metric"]["total_msgs_sent"])
-                total_bytes = int(el["metric"]["total_bytes_sent"])
-                int_msgs = int(el["metric"]["msgs_sent"])
-                int_bytes = int(el["metric"]["bytes_sent"])
-                data_dct[x] = {"exec_time": y, "total_msgs": total_msgs,
-                           "total_bytes": total_bytes, "msgs": int_msgs, "bytes": int_bytes}
-
-        else:
-            #y = str(float(el["value"][1])).replace(".", ",")
-            y = float(el["value"][1])
-            total_msgs = int(el["metric"]["total_msgs_sent"])
-            total_bytes = int(el["metric"]["total_bytes_sent"])
-            int_msgs = int(el["metric"]["msgs_sent"])
-            int_bytes = int(el["metric"]["bytes_sent"])
-            data_dct[x] = {"exec_time": y, "total_msgs": total_msgs,
-                           "total_bytes": total_bytes, "msgs": int_msgs, "bytes": int_bytes}
-
-        
-    for res in data_dct:
-        y = float(el["value"][1])
-        y = str(data_dct[res]["exec_time"]).replace(".", ",")
-        total_msgs = data_dct[res]["total_msgs"]
-        total_bytes = data_dct[res]["total_bytes"]
-        int_msgs = data_dct[res]["msgs"]
-        int_bytes = data_dct[res]["bytes"]
-        data_points.append({ "req": res, "exec_time": y, "total_msgs": total_msgs,
-                             "total_bytes": total_bytes, "msgs": int_msgs, "bytes": int_bytes})
+    for i in range(len(exec_time_asc)):
+        if int(exec_time_asc[i]["metric"]["state_length"]) != int(msgs_sent_asc[i]["metric"]["state_length"]):
+            raise ValueError("Results not matching")
+        x = int(exec_time_asc[i]["metric"]["state_length"])
+        exec_time = str(float(exec_time_asc[i]["value"][1])).replace(".", ",")
+        msgs_sent = int(msgs_sent_asc[i]["value"][1])
+        bts_sent = int(bts_sent_asc[i]["value"][1])
+        data_points.append({"req": x, "exec_time": exec_time,
+                            "msgs_sent": msgs_sent, "bytes_sent": bts_sent})
 
     # build key:val pairs for data points and return
     return data_points
 
 def main():
     logger.info("Starting data extraction for self-stabilization overhead experiment")
-    results = api.get_time_series_for_q(PROM_QUERY)
-    if len(results) == 0:
+    exec_time_series = api.get_time_series_for_q(Q_EXEC_TIME)
+    msgs_sent_time_series = api.get_time_series_for_q(Q_MSGS_SENT)
+    bytes_sent_time_series = api.get_time_series_for_q(Q_BYTES_SENT)
+
+    if len(exec_time_series) == 0:
         logger.warning("No results found, quitting")
         return
-    data_points = transform(results)
+    data_points = transform(exec_time_series, msgs_sent_time_series, bytes_sent_time_series)
     csv_path = helpers.write_to_csv(EXPERIMENT, data_points)
     snapshot_path = helpers.get_snapshot()
     helpers.collect_to_res_folder(EXPERIMENT, [csv_path, snapshot_path])
